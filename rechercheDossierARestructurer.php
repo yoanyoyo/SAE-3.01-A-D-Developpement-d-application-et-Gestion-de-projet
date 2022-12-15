97501166d7edf6682708172ee3731d55b5280664
Alexandre Ferreira <ferreira64240@hotmail.com>
2022-12-15 11:45:47 +0100

    Correction de l'algo 'restructurer'

--- rechercheDossierARestructurer.php
+++ rechercheDossierARestructurer.php
@@ -1,34 +1,55 @@
 <?php
+include_once "Fichier.php";
+include_once "Dossier.php";
+include_once "baseDeDonneePhysique.php";
 
 function RechercheDossierARestructurer($somme,$dossierParent,$listeObject,$trouver,$objetAPlacer){
-
+    $listEnfant = new \SplObjectStorage();
+    echo 'test';
+    echo '<br>';
+    $listEnfant->addAll($dossierParent->getListeEnfantFichier());
     if ($trouver == true) {
+        echo "trouvé";
         //Fin procedure
         return;
-    }elseif ($dossierParent->getListeEnfantFichier()!= NULL) {
+ 
+    }elseif ($listEnfant->valid()) {
         //Debut de la recherche 
-        $nbEnfant = sizeof($dossierParent->getListeEnfantFichier());
-
-        for ($enfant=0; $enfant < $nbEnfant-1; $enfant++) { 
-            $somme = $somme + $dossierParent->getListeEnfantFichier()[$enfant]->getTaille();
+        echo "recherche";   
+        while ($listEnfant->valid()) { 
+            $somme = $somme + $listEnfant->current()->getTaille();
             
-            //Test si on a trouver tout nos fichier
-            if ($somme > $objectAPlacer->getTaille()) {
+            //Test si on a trouvé tous nos fichier
+            if ($somme > $objetAPlacer->getTaille()) {
+                //if ($_POST["oui"]) (pour après avec les boutons)
                 $trouver = true;
                 break;
             }
         }
-
+        echo 'rt';
         //Initialisation du/des fichier(s) a restructurer et mise a jour du pointeur
-        $listeObject->attach($dossierParent->getListeEnfantFichier()[$enfant]);
-        $dossierParent->supprimerEnfant($enfant);
-
-        //Recherche avec les enfants
-        RechercheDossierARestructurer($somme,$dossierParent,$listeObject,$trouver,$objetAPlacer);
+        $listeObject->attach($listEnfant->current());
+        $dossierParent->supprimerEnfant($listEnfant->current());
+    }
+    //Recherche avec les enfants
+    echo 'si t triste';
+    $listEnfantDossier = $dossierParent->getListeEnfantDossier();
+    var_dump($listEnfantDossier);
+    if (isset($listEnfantDossier)) {
+        while ($listEnfantDossier->valid()) {
+            $dossierParent = $listEnfantDossier->current();
+    RechercheDossierARestructurer($somme,$dossierParent,$listeObject,$trouver,$objetAPlacer);
+    $listEnfantDossier->next();
+        }
     }
 }
 
-
-
-RechercheDossierARestructurer(0,$dossier1, ,false,$objetAPlacer);
+$listeObject = new \SplObjectStorage();
+$listeObject->attach($drive);
+$listeObject->attach($cloud);
+$listeObject->attach($FTP);
+$maSomme = 0;
+$trouve = false;
+RechercheDossierARestructurer($maSomme,$dossier1,$listeObject ,$trouve,$objetAPlacer);
+echo"finish";
 ?>
